7ed229ccb3c99c5d8de67438903e19bea65eabfe
Ola Fornal <aleksandra.goluch89@gmail.com>
2022-02-04 21:35:11 +0100

    base ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {CssBaseline, NextUIProvider} from "@nextui-org/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <NextUIProvider>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </NextUIProvider>
+    );
 }
 
 export default MyApp
