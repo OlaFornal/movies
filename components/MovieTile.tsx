f12649b1cd3bef7d6c0e731e568634f69131d633
Ola Fornal <aleksandra.goluch89@gmail.com>
2022-02-11 01:38:23 +0100

    [FIX] Broken import

--- components/MovieTile.tsx
+++ components/MovieTile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Button, Card, Col, Grid, Row, Text} from "@nextui-org/react";
-import {MovieInterface} from "../dataProvider/TheMovieDB/movies";
+import {MovieInterface} from "../types/tmdb.movies.types";
 
 interface MovieProps {
     movie: MovieInterface
